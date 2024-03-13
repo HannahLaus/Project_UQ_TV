70a463b8d7f84255e694476f5bf164590a132bd7
HannahLaus <100988088+HannahLaus@users.noreply.github.com>
2024-03-13 21:58:53 +0100

    Update compute_sample_covariance.py

--- compute_sample_covariance.py
+++ compute_sample_covariance.py
@@ -14,9 +14,7 @@ sampling = "radial"
 if sampling == "radial":
     mask = np.load("radial_mask.dat", allow_pickle=True) #exchange for the right mask
 elif sampling == "spiral":
-    mask = scipy.io.loadmat("k_space_mask_cart_w_edges_spiral_undersampling_factor_3.mat") #exchange for the right mask
-    mask = mask['k_space_mask']
-    mask = mask[50:206, 50:206]
+    mask = np.load("k_space_mask_spiral_undersampling_factor_3.0.npy")
 else:
     print('Matrixtype not available')
     raise Exception
@@ -31,6 +29,6 @@ nxysub = P.shape[0]
 F = pylops.signalprocessing.FFT2D(dims=(ny, nx), norm="none", fftshift_after=True, ifftshift_before=True)
 
 """Compute sample covariance and save it"""
-samplecovariance = 1/nxysub*(R*F).H*R*F*np.eye(nx*nx)
+samplecovariance = 1/nxysub*(P*F).H*P*F*np.eye(nx*nx)
 samplecovariance.dump("samplecov_radial.dat", protocol=4)
 
